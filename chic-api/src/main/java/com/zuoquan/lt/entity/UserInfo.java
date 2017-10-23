963895839a0fc1056e3de5f93d70de70bc689970
liutao <liutaowj46400@touna.cn>
2017-10-23 15:39:46 +0800

    添加依赖

---
+++ chic-api/src/main/java/com/zuoquan/lt/entity/UserInfo.java
@@ -0,0 +1,37 @@
+package com.zuoquan.lt.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by xinyi on 2017/10/9.
+ */
+public class UserInfo implements Serializable{
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
