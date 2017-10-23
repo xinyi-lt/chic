13e9eaac3a5a2eb490467e228522d8c1da4045a5
liutao <liutaowj46400@touna.cn>
2017-10-23 15:23:42 +0800

    初始化，项目结构创建

---
+++ chic-service/src/main/java/com/zuoquan/lt/ChicServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zuoquan.lt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicServiceApplication.class, args);
+	}
+}
---
+++ chic-web-server/src/main/java/com/zuoquan/lt/web/ChicWebServerApplication.java
@@ -0,0 +1,12 @@
+package com.zuoquan.lt.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicWebServerApplication.class, args);
+	}
+}
