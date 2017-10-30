181ed47468fc6c2dd60582bc32316468f0628e93
liutao <liutaowj46400@touna.cn>
2017-10-30 15:13:25 +0800

    注释修改

--- chic-web-server/src/main/java/com/zuoquan/lt/web/controller/UserInfoController.java
+++ chic-web-server/src/main/java/com/zuoquan/lt/web/controller/UserInfoController.java
@@ -35,7 +35,7 @@ public class UserInfoController {
      * 查询用户列表
      * @return
      */
-    @ApiOperation(value="获取用户列表", notes="")
+    @ApiOperation(value="获取用户列表", notes="查询全部用户信息")
     @RequestMapping(value="/list", method=RequestMethod.GET)
     public List<UserInfo> getUserList() {
         // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
